75ecc7dc669f8b6971682501b47e08e5c6f63465
Oleg Zadokhin <zadokhino@gmail.com>
2016-12-17 12:50:57 +0600

    Gem Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :places, dependent: :destroy 
 	has_many :ratings, dependent: :destroy 
 	has_many :photos, dependent: :destroy 
+	validates :name, presence: true, length: {maximum: 50}
 end
